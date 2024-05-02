422c2f9239f3397a5fde2a8acaf1ec5d96441eb4
manu09810 <manulorenzo19@gmail.com>
2024-05-01 21:21:47 -0300

    Correji el decimal_binario.py, lo optimize más.

---
+++ Clases/Ejercicio/Representación numerica/binario_decimal.py
@@ -0,0 +1,6 @@
+binario=int(input("Introduce tu número binario \n"))
+potenciador = 0
+while ()
+    potenciador=potenciador+1
+    decimal= (binario%10)*2**potenciador
+    binario= binario//10
--- Clases/Ejercicio/Representación numerica/decimal_binario.py
+++ Clases/Ejercicio/Representación numerica/decimal_binario.py
@@ -1,11 +1,7 @@
 n_decimal=int(input("Introduce tu número decimal \n"))
-cociente_0= False
 binario= " "
-while (cociente_0==False):
+while (n_decimal>0 ):
     binario =str(n_decimal % 2) + binario
     n_decimal= n_decimal//2
     print(n_decimal)
-    if (n_decimal ==0):
-        cociente_0=True
-
 print(binario)
